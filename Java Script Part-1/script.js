1344212d405c5c0f213dbaf26f4666d39260ef7d
YashSavaliya-Weybee <108047426+YashSavaliya-Weybee@users.noreply.github.com>
2022-06-23 12:07:27 +0530

    Java Script (part-1)

---
+++ Java Script Part-1/script.js
@@ -0,0 +1,75 @@
+// // //Coding Challenge - 1
+// console.log("Codng Challenge - 1");
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+// const BMIMark = massMark / (heightMark ** 2);
+// const BMIJohn = massJohn / (heightJohn ** 2);
+// const markHigherBMI = BMIMark > BMIJohn;
+// console.log(BMIMark, BMIJohn, markHigherBMI);
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+// //Coding Challenge - 2
+// console.log("Codng Challenge - 2");
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+// // const massMark = 95;
+// // const heightMark = 1.88;
+// // const massJohn = 85;
+// // const heightJohn = 1.76;
+// const BMIMark = massMark / (heightMark ** 2);
+// const BMIJohn = massJohn / (heightJohn **2);
+// console.log(BMIMark, BMIJohn);
+// if (BMIMark > BMIJohn) {
+//   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+// } else {
+//   console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`)
+// }
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+// //Coding Challenge - 3
+// console.log("Codng Challenge - 3");
+
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+// if (scoreDolphins > scoreKoalas) {
+//   console.log('Dolphins win the trophy 🏆');
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log('Koalas win the trophy 🏆');
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log('Both win the trophy!');
+// }
+
+// BONUS 1
+// const scoreDolphins = (97 + 112 + 80) / 3;
+// const scoreKoalas = (109 + 95 + 50) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+//   console.log('Dolphins win the trophy 🏆');
+// } else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+//   console.log('Koalas win the trophy 🏆');
+// } else if (scoreDolphins === scoreKoalas && (scoreDolphins && scoreKoalas) >= 100) {
+//   console.log('Both win the trophy!');
+// } else {
+//   console.log('No one wins the trophy 😭');
+// }
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+// //Coding Challenge - 4
+// console.log("Codng Challenge - 4");
+
+// const bill = 275;
+// const tip = 50 <= bill && bill <= 300 ? bill * 0.15 : ( 300 < bill ) ? bill * 0.2 : 0;
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+//--------------------------------------------------------------------------------------------------------------------------------
