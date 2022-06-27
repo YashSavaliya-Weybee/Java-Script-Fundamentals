3096727e6c936ad3b4de1eb0b35f7afdc15a36bb
YashSavaliya-Weybee <108047426+YashSavaliya-Weybee@users.noreply.github.com>
2022-06-27 17:50:37 +0530

    comment

--- JavaScript Works Behind the Scenes/script.js
+++ JavaScript Works Behind the Scenes/script.js
@@ -206,18 +206,18 @@
 // marriedJessica = {};
 
 // // Copying objects
-const jessica2 = {
-  firstName: 'Jessica',
-  lastName: 'Williams',
-  age: 27,
-  family: ['Alice', 'Bob'],
-};
+// const jessica2 = {
+//   firstName: 'Jessica',
+//   lastName: 'Williams',
+//   age: 27,
+//   family: ['Alice', 'Bob'],
+// };
 
-const jessicaCopy = Object.assign({}, jessica2);
-jessicaCopy.lastName = 'Davis';
+// const jessicaCopy = Object.assign({}, jessica2);
+// jessicaCopy.lastName = 'Davis';
 
-jessicaCopy.family.push('Mary');
-jessicaCopy.family.push('John');
+// jessicaCopy.family.push('Mary');
+// jessicaCopy.family.push('John');
 
-console.log('Before marriage:', jessica2);
-console.log('After marriage: ', jessicaCopy);
+// console.log('Before marriage:', jessica2);
+// console.log('After marriage: ', jessicaCopy);
